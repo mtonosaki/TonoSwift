95f341eeba55eb927e5db4b3c061ae6347fb64b3
mtonosaki <manabu@tomarika.com>
2021-12-27 17:24:51 +0900

    ver0.0.5

---
+++ Sources/Tono/TspResolver.swift
@@ -0,0 +1,96 @@
+// Tono (Tools Of New Operation) library
+//  MIT Lisence (c) 2021 Manabu Tonosaki all rights reserved
+//  Created by Manabu Tonosaki on 2021/12/27.
+
+import Foundation
+
+open class TspResolverLoop: TspResolver {
+
+    func solve(data: [TspNode]) -> [TspNode] {
+        nodes = Array(data)
+        guard var list = nodes else { fatalError()}
+        var indexes = (0..<list.count).map{ $0 }
+        let tarIndexes = (0..<(indexes.count - 1)).map{ indexes[$0 + 1] }
+        let res = optimize(indexArray: tarIndexes)
+        for i in 0..<res.count {
+            indexes[i + 1] = res[i]
+        }
+        let tmpNodes = Array(list)
+        list.removeAll()
+        for i in 0..<indexes.count {
+            list.append(tmpNodes[indexes[i]])
+        }
+        return list
+    }
+    
+    override func calcCost(indexArray: [Int]) -> Double {
+        guard let delegate = delegate else { fatalError("Set delegte first before solve") }
+        guard let nodes = nodes else {fatalError() }
+        
+        var ret = delegate.getTspCost(from: nodes[0], to: nodes[indexArray[0]], stage: .initialCost)
+        for i in 1..<indexArray.count {
+            ret += delegate.getTspCost(from: nodes[indexArray[i - 1]], to: nodes[indexArray[i]], stage: .normal)
+        }
+        ret += delegate.getTspCost(from: nodes[indexArray[indexArray.count - 1]], to: nodes[0], stage: .finalCostLoop)
+        return ret
+    }
+}
+
+public enum TspCaluclationStage
+{
+    case initialCost
+    case normal
+    case finalCostFix
+    case finalCostLoop
+}
+
+public protocol TspNode {
+}
+
+public protocol TspResolverDelegate {
+    func getTspCost(from: TspNode, to: TspNode, stage: TspCaluclationStage) -> Double
+}
+
+open class TspResolver {
+
+    final var delegate: TspResolverDelegate? = nil
+    internal var nodes: Array<TspNode>? = nil
+    
+    // Cost Calculator
+    func calcCost(indexArray: [Int]) -> Double {
+        fatalError("need implement this method")
+    }
+    
+    func optimize(indexArray: [Int]) -> [Int] {
+        var indexes = Array(indexArray)
+        let n = indexes.count
+        var cs = (0...n).map{ $0 }
+        var res = Array(indexes)
+        if indexes.count <= 0 {
+            return res
+        }
+        var rescost = calcCost(indexArray: indexes)
+        var i = 0
+        repeat {
+            let t = indexes[i]
+            let q = (i & 1) == 0 ? 0 : cs[i]
+            indexes[i] = indexes[q]
+            indexes[q] = t
+            
+            let cost = calcCost(indexArray: indexes)
+            if rescost > cost {
+                res = Array(indexes)
+                rescost = cost
+            }
+            i = 0
+            while  cs[i] == 0 {
+                cs[i] = i
+                i += 1
+            }
+            cs[i] -= 1
+            
+        } while i < n
+        
+        return res
+    }
+}
---
+++ Tests/TonoTests/TspResolverLoopTest.swift
@@ -0,0 +1,49 @@
+//
+//  File.swift
+//  
+//
+//  Created by Manabu Tonosaki on 2021/12/27.
+//
+
+import XCTest
+@testable import Tono
+
+class TspResolverLoopTest: XCTestCase, TspResolverDelegate {
+
+    func getTspCost(from: TspNode, to: TspNode, stage: TspCaluclationStage) -> Double {
+        guard let nodex = from as? Node else { fatalError() }
+        guard let nodey = to as? Node else { fatalError() }
+        
+        switch "\(nodex.name)\(nodey.name)" {
+            case "AB": return 6
+            case "BA": return 6
+            case "AC": return 5
+            case "CA": return 5
+            case "AD": return 5
+            case "DA": return 5
+            case "BC": return 7
+            case "CB": return 7
+            case "BD": return 4
+            case "DB": return 4
+            case "CD": return 3
+            case "DC": return 3
+            default: fatalError()
+        }
+    }
+    
+    
+    struct Node: TspNode {
+        var name: String
+    }
+    
+    func testLoop1() {
+        let tsp = TspResolverLoop()
+        tsp.delegate = self
+        let nodes = [Node(name: "A"), Node(name: "B"), Node(name: "C"), Node(name: "D")]
+        let ret = tsp.solve(data: nodes)
+        let retstr = ret.compactMap { $0 as? Node }.map{ $0.name }
+        let retjoin = retstr.joined(separator: "")
+        XCTAssertEqual(retjoin, "ABDC")
+    }
+}
+
